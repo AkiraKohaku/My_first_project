d6cba87b9253ed2b7993c7db44749f50384fef4a
AkiraKohaku <138078704+AkiraKohaku@users.noreply.github.com>
2023-06-28 18:53:53 -0600

    Add files via upload

---
+++ Byjus/SATemplate-main/SATemplate-main/miprimerprograma/sketch.js
@@ -0,0 +1,3 @@
+console.log("hola");
+console.log("*sonido de lamina de metal cayendo intensamente*");
+console.log("Humildemente opino que Roblox es un buen sistema de creación y disfrute de juegos, pero esta muy infravalorado");
